529b0650a30b3cf04538713625f57f7c23356910
yourUsername <yourEmail@domain.com>
2019-10-22 21:45:06 -0400

    MT

---
+++ src/MyThird.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MyThird {
+    public static void main(String args[]) {
+        System.out.println("Naomi!");
+        System.out.println("What's good homey.");
+        System.out.println("Siphara");
+        System.out.println("my girl");
+        Scanner keyboard = new Scanner(System.in);
+
+
+        int Exam1, Exam2, Exam3, Exam4;
+        System.out.println("Enter your first number:");
+        Exam1 = keyboard.nextInt();
+        System.out.println("Enter your second number:");
+        Exam2 = keyboard.nextInt();
+        System.out.println("Enter your third number:");
+        Exam3 = keyboard.nextInt();
+        System.out.println("Enter your fourth number:");
+        Exam4 = keyboard.nextInt();
+    }}
